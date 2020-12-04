283a5ffb99a22411a529f61e49e3b630c601f402
hosokawanaoki <hosokawamk2@gmail.com>
2020-12-04 22:37:22 +0900

    自動転送システム

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { App } = require('@slack/bolt');
+const moment = require("moment");
 const yaml = require('js-yaml');
 const fs = require('fs')
 const yamlText = fs.readFileSync('setting.yml', 'utf8')
@@ -13,32 +14,47 @@ const app = new App({
 
 (async () => {
     // Start your app
-    await app.start(process.env.PORT || 3000);
+    await app.start(process.env.PORT || 3001);
 
     console.log('⚡️ Bolt app is running!');
 })();
 
 
-app.message('hello', async ({ message, say }) => {
-    // say() sends a message to the channel where the event was triggered
-    await say(`Hey there <@${message.user}>!`);
-    sendToMychat(message)
+function sendToMychat(message) {
+    user(message).then((users) => {
+        let chatworkParams = {
+            chatworkToken: SETTING.TOKEN,
+            roomId: SETTING.ROOM_ID,
+            msg: editmessage(message, users)
+        };
+        chatwork.init(chatworkParams);
+        chatwork.postRoomMessages()
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    })
+
+}
+
+function editmessage(message, users) {
+
+    // ミリ秒切り捨てて時間に変換
+    var ts_str = moment.unix(Math.floor(message.ts)).local().toISOString()
+    message_result = message.text + '\n' + '-- ' + users.user.real_name + '  ' + ts_str
+    console.log(message_result);
+    return message_result;
+}
+
+app.message('', async ({ message, say }) => {
+    sendToMychat(message);
 });
 
-function sendToMychat() {
-    console.log(message);
-    let chatworkParams = {
-        chatworkToken: SETTING.TOKEN,
-        roomId: SETTING.ROOM_ID,
-        msg: 'Hello, i using chatwork-client'
-    };
-    chatwork.init(chatworkParams);
-    chatwork.postRoomMessages()
-        .then((data) => {
-            doSomething(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
+function user(data) {
+    return app.client.users.info({
+        token: SETTING.SLACK_BOT_TOKEN,
+        user: data.user
+    });
 }
